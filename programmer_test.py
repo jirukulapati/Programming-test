646f44c235a33ed60887f0fa2be866b4deb1fa6b
Jason Irukulapati <jsi012596@gmail.com>
2019-08-31 15:17:33 -0400

    Add files via upload

--- programmer_test.py
+++ programmer_test.py
@@ -6,8 +6,8 @@ con = sqlite3.connect("testdb.db")
 cur = con.cursor()
 
 #run an SQL query to find 10 people who have visited the most sites, insert it into frequent browsers
-#this query below only counts distinct sites visited by each person.
-cur.execute('''INSERT INTO frequent_browsers SELECT personID, COUNT(DISTINCT siteID) FROM visits GROUP BY personID ORDER BY COUNT(DISTINCT siteID)DESC LIMIT 10''')
+#this query below counts all sites visited by each person.
+cur.execute('''INSERT INTO frequent_browsers SELECT personID, COUNT(*) FROM visits GROUP BY personID ORDER BY COUNT(*)DESC LIMIT 10''')
 #db.commit()
 #used Pandas here because it's easier to print the results with pandas than with sqlite
 df = pd.read_sql_query("SELECT * FROM frequent_browsers", con)
@@ -15,4 +15,5 @@ df = pd.read_sql_query("SELECT * FROM frequent_browsers", con)
 #Print the table Here's the header
 
 print(df.to_string(index=False))
+
 
