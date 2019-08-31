49f6f67aad00867b2a0454adec54d231a77132c1
Jason Irukulapati <jsi012596@gmail.com>
2019-08-31 15:26:43 -0400

    Update app.py

--- Extras/test app/web/app.py
+++ Extras/test app/web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import sqlite3
+import pandas as pd
 
 import sqlite3
 app = Flask(__name__)
@@ -12,23 +13,14 @@ def startprogram():
 @app.route('/result', methods = ['GET'])
 def result():
     db = sqlite3.connect("testdb.db")
-    cur = db.cursor()
-    cur.execute("INSERT INTO frequent_browsers SELECT personID, COUNT(*) FROM visits GROUP BY personID ORDER BY COUNT(*)DESC LIMIT 10")
-    cur.execute("SELECT * FROM frequent_browsers")
-    a = ""
-    for column_name in cur.description:
-        a = a + column_name[0].ljust(20)
-    
-    a = a + "\n" + ('-' * 78)
+    con = con.cursor()
+    cur.execute('''INSERT INTO frequent_browsers SELECT personID, COUNT(DISTINCT siteID) FROM visits GROUP BY personID ORDER BY COUNT(DISTINCT siteID)DESC LIMIT 10''')
+    #db.commit()
+    #used Pandas here because it's easier to print the results with pandas than with sqlite
+    df = pd.read_sql_query("SELECT * FROM frequent_browsers", con)
+    df.to_string(index=False)
+    return(df.to_string(index=False)
 
-#heres the rows
-    column_indices = range(len(cur.description))
-    for row in cur:
-        b = ""
-        for column_index in column_indices:
-            b = b + str(row[column_index]).ljust(20)
-        a = a +"\n"+ b
-    return(a)
 
     
     
